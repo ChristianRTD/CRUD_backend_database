59a5cf946cffda8c81f69571f382dc0feac3d232
ChristianRTD <98784405+ChristianRTD@users.noreply.github.com>
2025-02-02 00:24:22 -0500

    feat: :bug: creation of backend routes

--- app/abaut/api/hello/route.js
+++ app/abaut/api/hello/route.js
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
-import { conn } from "@/libs/mysql";
 
-export async function GET() {
-  const result = await conn.query("SELECT NOW()");
-  console.log(result)
-  return NextResponse.json({ message: result[0]["now()"] });
+export  function GET() {
+  return NextResponse.json( {message: 'hello word'} );
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,15 @@
-import Navbar from "@/components/Navbar";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -12,9 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        <div className="h-[calc(100vh-5rem)] container mx-auto">{children}</div>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
       </body>
     </html>
   );
